1425d470db0049cf629cac6bfa965156799307a7
Jose Aguilar <jaguilarny@gmail.com>
2023-01-25 18:09:36 -0500

    TODO: creating/editing a post??

--- app/controllers/Api/posts_controller.rb
+++ app/controllers/Api/posts_controller.rb
@@ -3,4 +3,9 @@ class Api::PostsController < ApplicationController
     posts = Post.all
     render json: posts, status: :ok
   end
+
+  def show
+    post = Post.find_by(id: params[:id])
+    render json: post, status: :ok
+  end
 end
--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,5 +1,28 @@
-function Post() {
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
+function Post() {
+    const [post, setPost] = useState([])
+    const { id } = useParams();
+
+    useEffect(() => {
+        (async () => {
+            const res = await fetch(`/api/posts/${id}`)
+            if (res.ok) {
+                const post = await res.json()
+                setPost(post)
+                console.log(post)
+            }
+        })()
+    }, []);
+
+    return (
+        <div className="post">
+            <h1>{post.title}</h1>
+            <cite>Asked By {post.user_account_name}</cite>
+            <p>{post.body}</p>
+        </div>
+    )
 }
 
 export default Post;
