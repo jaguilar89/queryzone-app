456c3c6cef2ff1a4cf89b5b8d65bcdd760b6fbaf
Jose Aguilar <jaguilarny@gmail.com>
2023-03-01 16:32:28 -0500

    remove user_account_id field from Post table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true
 
-  belongs_to :user_account
   has_many :comments, dependent: :destroy
   has_many :user_accounts, through: :comments
 end
--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -3,6 +3,6 @@ class UserAccount < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :password, length: { minimum: 8 }
 
-  has_many :posts
   has_many :comments
+  has_many :posts, through: :comments
 end
