e54ac6c8128cf2ecb48098496daafd703765a919
Jose Aguilar <jaguilarny@gmail.com>
2023-08-09 12:44:07 -0400

    add new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
 require "faker"
 
-#for i in 1..10
-#  Post.create(title: Faker::Hacker.noun + " " + Faker::Hacker.verb + " " + Faker::Hacker.ingverb + "?",
-#              body: Faker::Lorem.paragraph(sentence_count: 10))
-#end
+for i in 1..10
+    UserAccount.create(username: "User#{i}", password: "welcome123", password_confirmation: "welcome123")
+end
 
-#for i in 1..10
-#  Comment.create(body: Faker::Hacker.say_something_smart, post_id: rand(1..10), user_account_id: 1)
-#end
+for i in 1..5
+  Post.create(title: Faker::Hacker.noun + " " + Faker::Hacker.verb + " " + Faker::Hacker.ingverb + "?",
+              body: Faker::Lorem.paragraph(sentence_count: 10), user_account_id: rand(1..10))
+end
+
+for i in 1..10
+  Comment.create(body: Faker::Hacker.say_something_smart, post_id: rand(1..5), user_account_id: rand(1..10))
+end
