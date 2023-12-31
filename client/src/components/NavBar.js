219602ed8147129add916ffa9d4242070c03f4d3
jaguilar89 <jaguilarny@gmail.com>
2023-12-31 13:56:48 -0500

    testing

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -93,16 +93,3 @@ function NavBar() {
 
 export default NavBar;
 
-
-/* const {setUser} = useContext(UserContext)
-
-
-async function handleLogout(e){
-    e.preventDefault();
-    
-    const res = await fetch('/api/logout', {
-        method: 'DELETE'
-    })
-    if (res.ok) setUser(null)
-}
-return <button onClick={handleLogout}>Logout</button> */
