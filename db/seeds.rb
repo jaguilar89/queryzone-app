4f242f73dcd82f67ee8b39e2116a5eb5e647c747
jaguilar89 <jaguilarny@gmail.com>
2024-01-01 14:49:47 -0500

    fix schema conflicts in dev and production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 require "faker"
 
-for i in 1..10
-    UserAccount.create(username: "User#{i}", password: "welcome123", password_confirmation: "welcome123")
+for i in 1..15
+  UserAccount.create(username: Faker::Internet.username(specifier: 5..10), password: "welcome123", password_confirmation: "welcome123")
 end
 
-for i in 1..5
+for i in 1..10
   Post.create(title: Faker::Hacker.noun + " " + Faker::Hacker.verb + " " + Faker::Hacker.ingverb + "?",
-              body: Faker::Lorem.paragraph(sentence_count: 10), user_account_id: rand(1..10))
+              body: Faker::Lorem.paragraph(sentence_count: 10), user_account_id: rand(1..15))
 end
 
-for i in 1..10
-  Comment.create(body: Faker::Hacker.say_something_smart, post_id: rand(1..5), user_account_id: rand(1..10))
+for i in 1..15
+  Comment.create(body: Faker::Hacker.say_something_smart, post_id: rand(1..10), user_account_id: rand(1..15))
 end
